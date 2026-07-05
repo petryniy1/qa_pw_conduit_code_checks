6eb80439cac6fdfe9d99e59945db3a9dbcf7a68b
Anton Sialitski <selitskiy.anton@gmail.com>
2026-07-05 16:15:19 +0200

    fix3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,33 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import playwright from 'eslint-plugin-playwright';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { languageOptions: { globals: globals.node } },
+  eslintConfigPrettier,
   {
-    ignores: [
-      'node_modules',
-      'dist',
-      'playwright-report',
-      'playwright.config.js',
-    ],
-  },
-
-  {
-  ...pluginJs.configs.recommended,
-    languageOptions: {
-      globals: globals.node,
-    },
-  },
-  
-  {
+    ...pluginJs.configs.recommended,
     ...playwright.configs['flat/recommended'],
-  },
-
-  {
     rules: {
+      ...pluginJs.configs.recommended.rules,
       'no-unused-vars': 'error',
       'max-len': [
         'error',
         {
           code: 80,
           comments: 80,
+          ignorePattern: 'import *',
         },
       ],
+      ...playwright.configs['flat/recommended'].rules,
+      'playwright/expect-expect': 'off',
     },
+    ignores: [
+      '**/node_modules/*',
+      'playwright.config.js',
+      '**/playwright-report/**',
+    ],
   },
 ];
